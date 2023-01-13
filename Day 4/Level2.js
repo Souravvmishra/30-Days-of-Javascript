474773374606ede50467195839ff7460cef3358a
SouravvMishra <souravvmishra@gmail.com>
2023-01-13 21:44:56 +0530

    Day 4 completed

---
+++ Day 4/Level2.js
@@ -0,0 +1,38 @@
+
+let Question
+
+// Question 1 
+// let mark = Number(prompt("Enter Your Marks"))
+// switch (true) {
+//     case mark > 100:
+//         console.log("Entered Value Is Not Valid");
+//         break;
+//     case mark > 80:
+//         console.log("You Got An A");
+//         break;
+//     case mark > 70:
+//         console.log("You Got An B");
+//         break;
+//     case mark > 60:
+//          console.log("You Got An C");
+//         break;
+//     case mark > 50:
+//         console.log("You Got An D");
+//         break;
+//     default:
+//         console.log("You Got An F");
+//         break;
+// }
+
+
+
+
+// Question 2
+let season = prompt("Enter a Month")
+if (season == "September" || season == "October" || season == "November" ) {
+    console.log("the season is Autumn.")
+} else if(season == "December" || season == "January " || season == "February") {
+    console.log("the season is Spring.")
+}else if(season == "June" || season == "July" || season == "August") {
+    console.log("the season is Summer.")
+}
---
+++ Day 4/level1.js
@@ -0,0 +1,32 @@
+// let Question 
+
+// Question 2
+// let age1 = prompt("Enter My Age", 0)
+// let age2 = prompt("Enter Your Age", 0)
+// if (age1 > age2) {
+//     console.log(`I am ${age1 - age2} years older than you`)
+// } else if(age1 < age2) {
+//     console.log(`You are ${age2 - age1} years older than me`)
+// }else{
+//     console.log(`We Both Are Same in age`)
+    // }
+  
+    
+// Question 3
+// let a = 4
+// let b = 3
+// if (a > b) {
+//     console.log('a is greater than b')
+// } else {
+//     console.log('a is less than b')
+// }
+
+// Question 4
+// let num = prompt("Enter a Number", 0)
+// if (num % 2 == 0) {
+//     console.log(`${num} is an even number`)
+// } else {
+//     console.log(`${num} is an odd number`)
+// }
+
+
